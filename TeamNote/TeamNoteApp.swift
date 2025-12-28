fb565e80e1ea5f7963c4c7e533d7647cb8389c5f
omkar vinay s <sanamreddyomkarvinay3104@gmail.com>
2025-12-28 19:09:37 +0530

    Initial Commit

---
+++ TeamNote/TeamNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamNoteApp.swift
+//  TeamNote
+//
+//  Created by sudheer korasika on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TeamNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
